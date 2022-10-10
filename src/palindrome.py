8a5a782a65cd3c398eaa8ee37efbc3ae4fc67ac7
Andrew Fennell <andrew.fennell1999@me.com>
2022-10-10 09:50:36 -0500

    Added src and palindrome

---
+++ src/palindrome.py
@@ -0,0 +1,46 @@
+import argparse
+
+def isPalindrome(s: str):
+    l, r = 0, len(s) - 1
+    mistakes = 0
+
+    while l <= r:
+        if s[l] == s[r]:
+            l += 1
+            r -= 1
+        else: # mistakes
+            mistakes += 1
+            if mistakes > 1:
+                return False
+            
+            # case 1: increment left
+            l += 1
+            if s[l] == s[r]:
+                continue
+            l -= 1
+            # case 2: decrement right
+            r -= 1
+            if s[l] == s[r]:
+                continue
+            return False
+    return True
+
+def main():
+    parser = argparse.ArgumentParser(description="Check if a string is a palindrome")
+    parser.add_argument("string", type=str)
+
+    args = parser.parse_args()
+    if isPalindrome(args.string):
+        print(f"{args.string} is a palindrome!")
+    else:
+        print(f"{args.string} is not a palindrome...")
+
+    
+
+if __name__ == "__main__":
+    """
+    sample = "racecar"
+    sample2 = "race car"
+
+    print(isPalindrome(sample))
+    """
